7717de965cd0d911430f4c2c1014ec3da0abfb1c
jchitranshi <jain.chixy005@gmail.com>
2019-03-04 17:58:23 +0530

    Add files via upload
    AddCourseFiles

--- My-University/src/main/java/com/restapi/myuniversityapi/courses/Course.java
+++ My-University/src/main/java/com/restapi/myuniversityapi/courses/Course.java
@@ -1,5 +1,54 @@
 package com.restapi.myuniversityapi.courses;
 
-public class Course {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+//import javax.persistence.ManyToOne;
 
+import com.restapi.myuniversityapi.topics.Topic;
+
+@Entity
+public class Course {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="student_id")
+	private long id;
+	private String name;
+	
+
+	public Course() {
+		
+	}
+	
+	public Course(long id, String name, Topic topic) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", name=" + name + "]";
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	
 }
